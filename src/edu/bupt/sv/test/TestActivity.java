0f193057da7f599c5de7cb1ad9045b65236f1cdc
buptlimeng <1006887071@qq.com>
2015-05-13 15:17:24 +0800

    test

--- src/edu/bupt/sv/test/TestActivity.java
+++ src/edu/bupt/sv/test/TestActivity.java
@@ -86,7 +86,7 @@ public class TestActivity extends Activity {
 		
 		quitBtn = (Button) findViewById(R.id.quit_btn);
 		quitBtn.setOnClickListener(listener);
-		
+		//test
 		init();
 	}
 
@@ -95,17 +95,5 @@ public class TestActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.test, menu);
 		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
